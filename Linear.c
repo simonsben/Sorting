4d7e91b6f0578109191850070222caeb440a7b2e
simonsben <simons.ben0@gmail.com>
2016-12-30 19:16:50 +0000

    Linear sort

---
+++ Linear.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+#define LEN 100000
+
+void prnt(float a[]) {
+	int i;
+	printf("Print: \n");
+	for(i=0;i<LEN;i++)
+		printf("%f\n", a[i]);
+}
+
+void gen(float a[]) {
+	int i;
+	for(i=0;i<LEN;i++)
+		a[i] = rand() % LEN;
+}
+
+int main() {
+	int i, j, sw;
+	float a[LEN], hld;
+
+	gen(a);
+	//prnt(a);
+
+	for(i=LEN;i>0;i--) {
+		sw = i;
+		for(j=0;j<i;j++) {
+			if(a[sw] < a[j])
+				sw = j;
+		}
+		if(sw != i) {
+			hld = a[sw];
+			a[sw] = a[i];
+			a[i] = hld;
+		}
+	}
+
+	//prnt(a);
+
+
+	return 0;
+}
