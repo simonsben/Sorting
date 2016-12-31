e984029fcd1fa082dd063f8f33e31435836c086c
simonsben <simons.ben0@gmail.com>
2016-12-31 11:24:53 -0500

    Insert

---
+++ Insert.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define LEN 100000
+
+void gen(float a[]) {
+	int i;
+	for(i=0;i<LEN;i++)
+		a[i] = rand() % 10;
+}
+
+void prnt(float a[]) {
+	int i;
+	printf("Print: \n");
+	for(i=0;i<LEN;i++)
+		printf("%.2f\n", a[i]);
+}
+
+void swap(float a[], int b, int c)  {
+    float sw = a[b];
+    a[b] = a[c];
+    a[c] = sw;
+}
+
+int main() {
+    int i, j;
+    float a[LEN], sw;
+
+    gen(a);
+    //prnt(a);
+    
+    for(i=1;i<LEN;i++) {
+        for(j=i;j>0;j--) {
+            if(a[j] < a[j-1])
+                swap(a, j, j-1);
+        }
+    }
+
+    //prnt(a);
+
+    return 0;
+}
