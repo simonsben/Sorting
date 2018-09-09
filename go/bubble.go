6ab981211a055d158da351de577f6c1b33b047f6
simonsben <simons.ben0@gmail.com>
2018-09-09 17:58:27 -0400

    Got bubble to work in go

--- go/bubble.go
+++ go/bubble.go
@@ -1,5 +1,17 @@
 package main
 
 func bubbleSort(data []float64) {
+	for i := len(data) - 1; i > 0; i-- {
+		noSwap := true
 
+		for j := 0; j < i; j++ {
+			if data[j] > data[j+1] {
+				swap(data, j, j+1)
+				noSwap = false
+			}
+		}
+		if noSwap {
+			break
+		}
+	}
 }
--- go/time.go
+++ go/time.go
@@ -8,10 +8,26 @@ import (
 var lengths = []int{100, 1000, 10000, 100000}
 
 func main() {
+	fmt.Printf("Starting tests")
 	testLinear()
+	testBubble()
+}
+
+func testBubble() {
+	fmt.Printf("\nStarting bubble sort\n")
+	for i := 0; i < len(lengths); i++ {
+		dataSet := generateNums(lengths[i])
+
+		start := time.Now()
+		bubbleSort(dataSet)
+		elapsed := time.Since(start)
+
+		fmt.Printf("Time for n=%d %.5fs\n", lengths[i], elapsed.Seconds())
+	}
 }
 
 func testLinear() {
+	fmt.Printf("\nStarting linear sort\n")
 
 	for i := 0; i < len(lengths); i++ {
 		dataSet := generateNums(lengths[i])
